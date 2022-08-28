2b84a6dd11cc7e2714f3b4589d06a339478153da
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-08-28 16:16:18 +0530

    used constructors

---
+++ MoodAna.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moodanalyzer
+{
+    public class MoodAnalyzer
+    {
+        private const string message = "I am in Sad Mood";
+        private string testmessage = "";
+        //public string Message
+        //{ 
+        //    get { return message; } 
+        //    set { message = value; }
+        //}
+        public MoodAnalyzer()
+        {
+
+        }
+        public MoodAnalyzer(string msg)
+        {
+            this.testmessage = msg;
+        }
+
+        public string AnalyseMood()
+        {
+            if (this.testmessage == message)
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "Happy";
+            }
+        }
+    }
+}
