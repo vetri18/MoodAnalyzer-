1cbf99416eee2c2b64bb3518904e28d03fcb78f5
vetrivel <vetrivel.kce@gmail.com>
2022-08-28 16:12:10 +0530

    welcome to mood analyzer problem

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MoodAnalyzer
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyzer problem");
+        }
+    }
+}
