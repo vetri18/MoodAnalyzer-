904ba83afe5b48e8c31ec9a35b00d61b6264ccfe
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-08-28 16:17:02 +0530

    null string exception handled uc 3

---
+++ MoodAnaly.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moodanalyzer
+{
+    public class MoodAnalyzer
+    {
+        private const string message = "I am in Sad Mood";
+        private string testmessage = "";
+        //public string Message
+        //{ 
+        //    get { return message; } 
+        //    set { message = value; }
+        //}
+        public MoodAnalyzer()
+        {
+
+        }
+        public MoodAnalyzer(string msg)
+        {
+            this.testmessage = msg;
+        }
+
+        public string AnalyseMood()
+        {
+
+            try
+
+            {
+                if (this.testmessage == "NULL" || this.testmessage == string.Empty)
+                {
+                    throw new Exception("Mood is Null");
+                }
+
+                if (this.testmessage == message)
+                {
+                    return "SAD";
+                }
+                else if (this.testmessage == "I am in Happy Mood")
+                {
+                    return "Happy";
+                }
+                else
+                {
+                    throw new Exception("Mood is Null");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return "Happy";
+            }
+
+
+        }
+    }
+}
