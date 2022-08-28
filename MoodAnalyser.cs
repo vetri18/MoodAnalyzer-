f7253a02310ad8b89904bd370b420db7ed0bfeef
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-08-28 16:15:25 +0530

    checking mood

---
+++ MoodAnalyser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moodanalyzer
+{
+    public class MoodAnalyzer
+    {
+        public string message = "I am in Sad Mood";
+        public string AnalyseMood(string msg)
+        {
+            if (this.message == msg)
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "Happy";
+            }
+
+        }
+    }
+}
---
+++ UnitClass.test.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moodanalyzer;
+
+namespace MoodAnalyzerTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AnalyseMood()
+        {
+            //Arrange
+            MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+            string meassage = "I am in Sad Mood";
+            string ExpectedOutput = "SAD";
+
+            string result = moodAnalyzer.AnalyseMood(meassage);
+
+            Assert.AreEqual(result, ExpectedOutput);
+
+        }
+    }
+}
